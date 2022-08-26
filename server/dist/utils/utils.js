b16cdc731c2cc5d9b62fa976cb02a79975bfc10d
dannack <felixtemikotan@yahoo.com>
2022-08-26 17:05:41 +0100

    user register backend - felix

--- server/dist/utils/utils.js
+++ server/dist/utils/utils.js
@@ -1,1 +1,27 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.options = exports.registerSchema = void 0;
+const joi_1 = __importDefault(require("joi"));
+exports.registerSchema = joi_1.default.object()
+    .keys({
+    fullName: joi_1.default.string().required(),
+    email: joi_1.default.string().trim().lowercase().required(),
+    phoneNumber: joi_1.default.string()
+        .length(11)
+        .pattern(/^[0-9]+$/)
+        .required(),
+    password: joi_1.default.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+    confirm_password: joi_1.default.ref("password"),
+})
+    .with("password", "confirm_password");
+exports.options = {
+    abortEarly: false,
+    errors: {
+        wrap: {
+            label: "",
+        },
+    },
+};
