fb99a0730458294c00e148c011725f45f0efc7cb
Imeh Usoro <imeh.usoro@decagon.dev>
2022-08-26 01:24:54 +0100

    database init

--- server/dist/app.js
+++ server/dist/app.js
@@ -9,6 +9,7 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const index_1 = __importDefault(require("./routes/index"));
 const users_1 = __importDefault(require("./routes/users"));
+const database_config_1 = __importDefault(require("./config/database.config"));
 const app = (0, express_1.default)();
 app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
@@ -17,4 +18,10 @@ app.use((0, cookie_parser_1.default)());
 app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
 app.use("/", index_1.default);
 app.use("/users", users_1.default);
+// db.sync({ force: true }) - to reset the database
+database_config_1.default.sync()
+    .then(() => {
+    console.log(`Database connected successfully`);
+})
+    .catch((err) => console.log(err));
 exports.default = app;
--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,7 @@ import logger from "morgan";
 
 import indexRouter from "./routes/index";
 import usersRouter from "./routes/users";
+import db from "./config/database.config";
 
 const app = express();
 
@@ -16,5 +17,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+
+// db.sync({ force: true }) - to reset the database
+db.sync()
+  .then(() => {
+    console.log(`Database connected successfully`);
+  })
+  .catch((err) => console.log(err));
 
 export default app;
