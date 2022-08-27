230c3e742c53a7341e44c59e4b9990a327aee2f8
DevEunice <obebeeunice@gmail.com>
2022-08-27 23:26:50 +0100

    login backend

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response, NextFunction } from "express";
 const router = express.Router();
-import { RegisterUser } from "../controller/userController";
+import { RegisterUser, LoginUser } from "../controller/userController";
 
 /* GET users listing. */
 router.get("/", function (req: Request, res: Response, next: NextFunction) {
   res.send("respond with a resource");
 });
 router.post("/register", RegisterUser);
+
+router.post("/login", LoginUser)
 
 export default router;
